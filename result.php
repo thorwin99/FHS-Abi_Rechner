103e72d0df9c895e64b47378daf159f6497fe37c
thorwin99 <thorwin99@gmail.com>
2017-06-11 14:16:12 +0200

    Added results

--- result.php
+++ result.php
@@ -39,21 +39,36 @@
                 $SUMPMarks = 0;//Summe anderer Fächer
                 $CountEUn = 0;//Anzahl unterkurse in den EA Fächern
                 $CountUn = 0;//Anzahl unterkurse in den anderen Fächern
-                foreach($_POST['emarks'] as $emark){
+                foreach($_POST['emarks'] as $emark){//Zähle die summe der e.A.Fächer
                     $SUMEmarks += $emark * 2;
                     if($emark <= 4){
                         $CountEUn++;
                     }
                 }
-                foreach($_POST['marks'] as $mark){
-                    $SUMPMarks += $mark * 2;
+                foreach($_POST['marks'] as $mark){//Zähle die Summe der anderen Fächer
+                    $SUMPMarks += $mark;
                     if($mark <= 4){
                         $CountUn++;
                     }
                 }
-                if($CountEUn >= $AllowedEUn){
+                if($CountEUn >= $AllowedEUn){//Prüfe ob zu viele e.A. Unterkurse
                     echo getHTMLObject("h2", array("id" => "resultString"), "Zu viele e.A. Unterkurse: " . $CountEUn);
+                }else{
+                    if($CountUn + $CountEUn >= $AllowedUn){//Prüfe ob zu viele Unterkurse
+                        echo getHTMLObject("h2", array("id" => "resultString"), "Zu viele Unterkurse: " . ($CountEUn + $CountUn));
+                    }else{
+                        if($SUMEmarks < $MinEASum){//Prüfe ob genug e.A. Punkte
+                            echo getHTMLObject("h2", array("id" => "resultString"), "Zu wenig e.A. Punkte: " . $SUMEmarks . " / " . $MinEASum);
+                        }else{
+                            if($SUMPMarks + $SUMEmarks < $MinMarkSum){//Prüfe ob genug Punkte
+                                echo getHTMLObject("h2", array("id" => "resultString"), "Zu wenig Punkte: " . ($SUMEmarks + $SUMPMarks). " / " . $MinMarkSum);
+                            }else{//Zeige Ergebnis
+                                echo getHTMLObject("h2", array("id" => "resultString"), "Bestanden mit " . ($SUMEmarks + $SUMPMarks) . " Punkten");
+                            }
+                        }
+                    }
                 }
+                
 
             }
         ?>
