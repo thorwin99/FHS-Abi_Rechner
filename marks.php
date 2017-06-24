f2007177837900ac3d92adce426534a1efe741e1
thorwin99 <thorwin99@gmail.com>
2017-06-24 15:57:42 +0200

    Fixed jQuery Bug

--- marks.php
+++ marks.php
@@ -16,6 +16,8 @@
 		</title>
 		 <meta charset="UTF-8"> 
 		<link rel="stylesheet" type="text/css" href="style/webStyle.css">
+        <!-- load jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	</head>
 	<body>
         <!--Noscript Redirect to enable-javascript.com if it isnt activated-->
